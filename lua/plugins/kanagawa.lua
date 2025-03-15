15debb5fdd5a11447e6682776b17aeccaf27e056
Anand Dubey <36227152+anandubey@users.noreply.github.com>
2025-03-16 00:36:25 +0530

    statusline, oil

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,9 +2,13 @@ return {
 	"rebelot/kanagawa.nvim",
 	config = function()
 		require('kanagawa').setup({
-			compile = true
+			compile = true,
+			background = {               -- map the value of 'background' option to a theme
+        dark = "dragon",           -- try "dragon" !
+        light = "lotus"
+    },
 		});
-		vim.cmd("colorscheme kanagawa-dragon")
+		vim.cmd("colorscheme kanagawa")
 	end,
 	build = function()
 		vim.cmd("KanagawaCompile");
